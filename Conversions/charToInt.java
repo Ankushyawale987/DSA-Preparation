25161fac3da0826159c1786b21488197a2fecdfe
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2025-03-06 12:28:20 +0530

    Create charToInt.java

---
+++ Conversions/charToInt.java
@@ -0,0 +1,46 @@
+public class Main{
+    public static void main(String[] args){
+        char c = '3';
+        int a = c - '0';
+        System.out.println("Integer value is:" + a);
+        System.out.println("Charcater value is:" + c);
+        
+        for(char i='a'; i<='z';i++){
+            System.out.println((int)i);
+        }
+    }
+}
+
+/*
+Output:-
+Integer value is:3
+Charcater value is:3
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+
+=== Code Execution Successful ===
+*/
