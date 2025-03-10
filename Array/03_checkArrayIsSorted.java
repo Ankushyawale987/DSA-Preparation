a1c14ed740690a7fd79f599e79418702e8d41229
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2025-03-10 12:48:41 +0530

    Check Array Is Sorted or Not

---
+++ Array/03_checkArrayIsSorted.java
@@ -0,0 +1,32 @@
+/*
+Case Study: 
+Array is given in non decending order means array is in Acending Order 
+So check the count of of next array element is small or not,
+if it is more than 1 then it is false 
+otherwise true.
+*/
+import java.util.*;
+class Main {
+    public boolean CheckSorted(int[] arr){
+        int c = 0;
+        for(int i=0;i<arr.length - 1;i++){
+            if(arr[i + 1] < arr[i]){
+                c++;
+            }
+        }
+        if(arr[0] < arr[arr.length - 1]){
+            c++;
+        }
+        return c<=1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+        Main ob = new Main();
+        boolean ans = ob.CheckSorted(arr);
+        System.out.println(ans);
+    }
+}
+
+//****//
+// Time Complexity: O(N) - Sorting of Array
+// Space Complexity: O(1)
